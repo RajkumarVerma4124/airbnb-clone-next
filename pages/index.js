ad8b044f584a5692e40a7ab2e8c0ba56a0c0093d
codered-jack <sumitnbq@gmail.com>
2021-08-05 09:36:33 +0530

    header

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ export default function Home({exploreData,cardsData}) {
   return (
     <div>
       <Head>
-        <title>Create Next App</title>
+        <title>AirBnb 2.0</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
